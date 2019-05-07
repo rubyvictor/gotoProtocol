e3434efd78db0168aa72f22f527d6765eac01947
victor lee ming yew <victor_li@flyscoot.com>
2019-05-07 12:33:55 +0800

    Initial Commit

---
+++ GoingProto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoingProto
+//
+//  Created by Victor Lee on 7/5/19.
+//  Copyright © 2019 VictorLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
